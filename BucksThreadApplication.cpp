e642e7ca27fb7ec0f03aa0e2df5393793acd40cf
thereconjacob <jvpokeyjake@gmail.com>
2023-02-17 15:10:49 +0000

    Do exercise 1

--- BucksThreadApplication.cpp
+++ BucksThreadApplication.cpp
@@ -4,46 +4,30 @@
 #include <iostream>
 #include <Windows.h>
 
-DWORD WINAPI BasicThread(LPVOID param) {
-    std::cout << "Thread start \n";
-    Sleep(2000);
-    std::cout << "Thread end \n";
-    return 0;
-}
-
-DWORD WINAPI MessageThread(LPVOID param) {
-    int tab = (int)param;
-    for (int n = 0; n < 10; n++) {
-        Sleep(50);
-        for (int i = 0; i < 100000; i++);
-        if (tab == 1)
-            std::cout << "\t\t 1:Thread\n";
-        else if (tab == 2)
-            std::cout << "\t\t\t\t 2:Thread\n";
-        else
-            std::cout << "\t\t\t\t\t\t 3:Thread\n";
+class GameObject {
+public:
+    int health;
+    GameObject() {
+        health = 100;
     }
+};
+
+DWORD WINAPI SyncThread(LPVOID param) {
+    GameObject* syncObj = (GameObject*)param; //Cast the LPVOID param back to being a GameObject but it is a pointer
+    std::cout << "Game Object Health : " << syncObj->health << "\n";
     return 0;
 }
 
 int main()
 {
-    LPVOID param = NULL;
     DWORD threadId;
-    HANDLE hdl[3];
-    int label = 1;
-    hdl[0] = CreateThread(NULL, 0, MessageThread, (LPVOID)label, 0, &threadId);
-    SetThreadPriority(hdl[0], THREAD_PRIORITY_IDLE); //This thread has a low priority
-    label = 2;
-    hdl[1] = CreateThread(NULL, 0, MessageThread, (LPVOID)label, 0, &threadId);
-    SetThreadPriority(hdl[1], THREAD_PRIORITY_TIME_CRITICAL); //This thread has the highest priority
-    label = 3;
-    hdl[2] = CreateThread(NULL, 0, MessageThread, (LPVOID)label, 0, &threadId);
-    SetThreadPriority(hdl[2], THREAD_PRIORITY_LOWEST); //This thread has the lowest priority
+    HANDLE hdl[2];
+    GameObject* obj = new GameObject();
+    hdl[0] = CreateThread(NULL, 0, SyncThread, (LPVOID)obj, 0, &threadId); //Again the obj variable needs to be cast to LPVOID to pass as a parameter for a thread
+    hdl[1] = CreateThread(NULL, 0, SyncThread, (LPVOID)obj, 0, &threadId);
     HANDLE mainThread = GetCurrentThread();
-    SuspendThread(mainThread);
+    SuspendThread(mainThread); //Main thread gets suspended, otherwise if it finishes, all child threads will stop
     system("pause");
-
-    //When return on the main function is called, all other children thread are stopped because the parent main thread is stopped
+    delete obj;
     return 0;
 }

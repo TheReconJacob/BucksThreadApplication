f7be302aacd21976d4f68aac2aa9fa51942787b2
thereconjacob <jvpokeyjake@gmail.com>
2023-02-17 15:31:58 +0000

    Do exercise 2

--- BucksThreadApplication.cpp
+++ BucksThreadApplication.cpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <Windows.h>
 
+char mutexName[] = "MUTEX1";
+HANDLE hMutex;
+
 class GameObject {
 public:
     int health;
@@ -14,7 +17,9 @@ public:
 
 DWORD WINAPI SyncThread(LPVOID param) {
     GameObject* syncObj = (GameObject*)param; //Cast the LPVOID param back to being a GameObject but it is a pointer
+    WaitForSingleObject(hMutex, INFINITE);
     std::cout << "Game Object Health : " << syncObj->health << "\n";
+    ReleaseMutex(hMutex);
     return 0;
 }
 
@@ -22,6 +27,7 @@ int main()
 {
     DWORD threadId;
     HANDLE hdl[2];
+    hMutex = CreateMutex(NULL, false, LPCWSTR(mutexName));
     GameObject* obj = new GameObject();
     hdl[0] = CreateThread(NULL, 0, SyncThread, (LPVOID)obj, 0, &threadId); //Again the obj variable needs to be cast to LPVOID to pass as a parameter for a thread
     hdl[1] = CreateThread(NULL, 0, SyncThread, (LPVOID)obj, 0, &threadId);
